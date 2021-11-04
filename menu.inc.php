aef6b3231cde11c750b3b1a40596c0157b11b429
archer62 <archer62g@gmail.com>
2021-11-04 21:17:27 +0300

    8.6. Практическая работа

---
+++ menu.inc.php
@@ -0,0 +1,7 @@
+            <nav>
+                <ul>
+                    <li><a href="#about_me"> Me </a></li>
+                    <li><a href="#about_me"> Myself </a></li>
+                    <li><a href="#about_me"> I </a></li>
+                </ul>    
+            </nav>
